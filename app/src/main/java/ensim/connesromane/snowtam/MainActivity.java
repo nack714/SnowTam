bd80d9a7285f7db862ff4168a8be9f5b8535e30c
Nicolas <nack714@hotmail.fr>
2019-11-20 18:03:09 +0100

    maps

--- app/src/main/java/ensim/connesromane/snowtam/MainActivity.java
+++ app/src/main/java/ensim/connesromane/snowtam/MainActivity.java
@@ -2,13 +2,37 @@ package ensim.connesromane.snowtam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText lat, lon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        lat = (EditText)findViewById(R.id.editText);
+        lon = (EditText)findViewById(R.id.editText2);
+        lat.setText("48.018851");
+        lon.setText("0.157715");
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                intent.putExtra("lat",  lat.getText().toString());
+                intent.putExtra("lon", lon.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/ensim/connesromane/snowtam/MapsActivity.java
@@ -0,0 +1,68 @@
+package ensim.connesromane.snowtam;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    float lat, lon;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        lat = Float.valueOf(intent.getStringExtra("lat"));
+        lon = Float.valueOf(intent.getStringExtra("lon"));
+
+        CharSequence text = "Name : "+lat+"\nSurname : "+lon;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(this, text, duration);
+        toast.show();
+
+
+
+
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+
+
+        // Add a marker in Sydney and move the camera
+        LatLng marker = new LatLng(lat, lon);
+        mMap.addMarker(new MarkerOptions().position(marker).title("Marker sur l'ENSIM"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(marker));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(marker, 16));
+    }
+}
