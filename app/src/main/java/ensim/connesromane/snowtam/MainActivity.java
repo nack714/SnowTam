d1b96e3f64ef9d15b3fb7cc63f321542960f59d9
Nicolas <nack714@hotmail.fr>
2019-11-21 08:47:44 +0100

    Ajout liste airoport

--- app/src/main/java/ensim/connesromane/snowtam/MainActivity.java
+++ app/src/main/java/ensim/connesromane/snowtam/MainActivity.java
@@ -2,14 +2,23 @@ package ensim.connesromane.snowtam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.LauncherActivity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     EditText lat, lon;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,5 +43,27 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        ListView listView = (ListView)findViewById(R.id.listView);
+
+        ArrayList<Airport> airportList = new ArrayList<>();
+        airportList.add(new Airport("PTP", "Pointe-à-Pitre"));
+        airportList.add(new Airport("NTE", "Nantes Atlantique"));
+        airportList.add(new Airport("CDG", "Président français disparu"));
+        airportList.add(new Airport("JFK", "Président américain disparu"));
+
+
+        // android.R.layout.simple_list_item_1 is a constant predefined layout of Android.
+        // used to create a ListView with simple ListItem (Only one TextView).
+
+        ArrayAdapter<Airport> arrayAdapter = new ArrayAdapter<Airport>(this, android.R.layout.simple_list_item_1 , airportList);
+
+
+        listView.setAdapter(arrayAdapter);
+
+
+
+
     }
+
+
 }
